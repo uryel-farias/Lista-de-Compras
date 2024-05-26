c2f8a3f7d751a55f449b3cd91e36d2fdf0856d76
Uryel Farias <uryel.farias@gmail.com>
2024-05-26 02:33:51 -0300

    Dando continuidade ao iniico do prajeto.

--- lista-de-compras-projeto-base/main.js
+++ lista-de-compras-projeto-base/main.js
@@ -1,1 +1,18 @@
 let listaDeItens = [];
+
+const form = document.getElementById("form-itens");
+const itensInput = document.getElementById("receber-item");
+
+form.addEventListener("submit", function(evento){
+    evento.preventDefault();
+})
+
+function salvarItem() {
+    const comprasItem = itensInput.value;
+
+    listaDeItens.push ({
+        valor: comprasItem
+    })
+
+
+}
