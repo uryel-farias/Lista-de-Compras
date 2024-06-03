f2b31ccc91d3439d4c4a5f62cfdb8c9f9137799d
Uryel Farias <uryel.farias@gmail.com>
2024-06-02 23:49:49 -0300

    Manipulando o DOM parte 1.

--- lista-de-compras-projeto-base/main.js
+++ lista-de-compras-projeto-base/main.js
@@ -2,8 +2,9 @@ let listaDeItens = [];
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
-
 const ulItens = document.getElementById("lista-de-itens");
+const ulItensComprados = document.getElementById("itens-comprados");
+
 
 form.addEventListener("submit", function(evento){
     evento.preventDefault();
@@ -31,8 +32,23 @@ function salvarItem() {
 
     function mostrarItem() {
         ulItens.innerHTML = ''
+        ulItensComprados.innerHTML = ''
     
         listaDeItens.forEach((elemento, index) => {
+            if (elemento.checar) {
+                ulItensComprados.innerHTML += `
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" checked class="is-clickable" />
+                    <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+                `
+            } else {
+                            
             ulItens.innerHTML += `
             <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
             <div>
@@ -42,8 +58,8 @@ function salvarItem() {
             <div>
                 <i class="fa-solid fa-trash is-clickable deletar"></i>
             </div>
-        </li>
-        `
+            </li>
+            `}
         })
 
         const inputCheck = document.querySelectorAll('input[type="checkbox"]');
@@ -52,7 +68,7 @@ function salvarItem() {
             i.addEventListener('click', (evento) => {
                 const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
                 listaDeItens[valorDoElemento].checar = evento.target.checked
-                console.log(listaDeItens[valorDoElemento].checar)
+
             });
         });
 
